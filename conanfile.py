a8951d534f79d135bc5e25386c00b0fee6c270a7
gukazma <gukazma@proton.me>
2023-12-03 22:21:47 +0800

    🔧 build: new build frame

--- conanfile.py
+++ conanfile.py
@@ -1,59 +1,20 @@
-from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conans import ConanFile, tools
+import platform
 
 
-class CppTemplateProjectRecipe(ConanFile):
-    name = "CppTemplateProject"
-    version = "1.0"
-    package_type = "library"
-
-    # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of hello package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
-
-    # Binary configuration
+class ModuleConan(ConanFile):
+    name = "Glim"
+    description = ""
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "Modules/*", "Modules/*"
+    generators = "cmake", "cmake_find_package_multi", "cmake_paths"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            self.options.rm_safe("fPIC")
+    def configure(self):
+        del self.settings.compiler.cppstd
+
     def requirements(self):
         self.requires("gtest/1.14.0")
         self.requires("sdl/2.28.5")
         self.options["sdl"].shared = True
         
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
-    def layout(self):
-        cmake_layout(self)
-
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-        tc = CMakeToolchain(self)
-        tc.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = ["CppTemplateProject"]
-
     def imports(self):
-        self.copy("*", "./build/bin", "bin")
+        self.copy("*.dll", "./bin", "bin")
